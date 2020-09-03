12963a0ec2eabbf27edb97c2bbbdaaa6d706013e
Vladimir-Molchamov <vovinator03@gmail.com>
2020-09-03 23:02:56 +0600

    second power

---
+++ power1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите что нужно ввести во вторую степень");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n * n);
+            Console.ReadKey();
+        }
+    }
+}
